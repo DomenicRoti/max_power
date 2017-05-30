7fefd24c7a297c290ea2190efaa556bc5411d7a9
DomenicRoti <domenic.roti@gmail.com>
2017-05-30 01:32:00 -0600

    Adding the ability to post a new company (although I am not requiring an auth token)

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,5 +1,10 @@
 class CompaniesController < ApplicationController
+  skip_before_action :verify_authenticity_token
   def index
+    render json: Company.all
+  end
+  def create
+    Company.create({'name' => params[:company]['name'] })
     render json: Company.all
   end
 end
